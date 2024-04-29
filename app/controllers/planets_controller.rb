d8e08365bfbfecc49e3f58780f2baea2af9ae0b1
Kelly Ekomie <kelly.ekomie@gmail.com>
2024-04-29 18:18:28 +0200

    Added the new, create and planet params methods in the planets_controller.rb

--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -4,9 +4,24 @@ class PlanetsController < ApplicationController
   def show
   end
 
+  def new
+    @planet = Planet.new
+  end
+
+  def create
+    @planet = Planet.new(planet_params)
+    if @planet.save
+      redirect_to planet_path(@planet)
+    else
+      render :new, status: :unprocessable_entity
+  end
+
   private
 
   def set_planet
     @planet = Planets.find[:id]
   end
+
+  def planet_params
+    params.require(:planet).permit(:name, :details, :location, :price_per_night, :planet_img)
 end
