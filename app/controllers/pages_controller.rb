4ee17e6684c52d05481a16beb2a39ed4e195535b
Chris Collis <theotteryears@me.com>
2024-05-02 21:19:40 +0100

    Add average rating to home page cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @reviews = Review.where(params[:planet_id])
+    @average_rating = @reviews.average(:rating)
     planets = Planet.all
     @planets = planets.shuffle.take(3)
   end
