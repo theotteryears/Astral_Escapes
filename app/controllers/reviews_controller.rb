834c5b52becdaede07e96028831fe4756bee20f6
Soraya Dos Santos <dossantos.soraya@gmail.com>
2024-05-02 11:13:50 +0200

    review_create

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,28 @@
 class ReviewsController < ApplicationController
+before_action :set_planet, only: %i[new create]
+
   def new
     @planet = Planet.find(params[:planet_id])
     @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    review.planet = @planet
+    if review.save
+    redirect_to planet_path(@planet)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_planet
+    @planet = Planet.find(params[:planet_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content)
   end
 end
