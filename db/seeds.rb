87d55cfc7146ba377fd4064c4bff89bb9618d207
Chris Collis <theotteryears@me.com>
2024-05-02 22:04:20 +0100

    Review seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -214,13 +214,33 @@ puts "Finished creating 38 planets"
 
 puts "---------------------------------"
 
-puts "Creating 3 bookings..."
+puts "Creating 4 bookings..."
 
 Booking.create!(visitor_id: 1, planet_id: 4, start_date: Date.new(2544,05,10), end_date: Date.new(2544,05,31), status: "Unconfirmed")
 Booking.create!(visitor_id: 1, planet_id: 20, start_date: Date.new(2544,06,22), end_date: Date.new(2544,07,22), status: "Unconfirmed")
 Booking.create!(visitor_id: 1, planet_id: 14, start_date: Date.new(2544,10,01), end_date: Date.new(2544,10,10), status: "Confirmed")
+Booking.create!(visitor_id: 1, planet_id: 5, start_date: Date.new(2545,01,03), end_date: Date.new(2545,02,04), status: "Confirmed")
 
-puts "Finished creating 3 bookings"
+puts "Finished creating 4 bookings"
+
+puts "---------------------------------"
+
+puts "Creating 6 reviews..."
+
+Review.create!(content: "My stay with Astral Escapes was nothing short of extraordinary! From the moment I arrived on the shimmering surface of Castila, I was captivated by its otherworldly beauty. The accommodations were beyond compare, and the sense of wonder I experienced exploring the alien landscapes will stay with me forever. A truly unforgettable cosmic getaway!", rating: 5, booking_id: 1)
+
+Review.create!(content: "Astral Escapes offered a thrilling adventure, but an unexpected giant lizard encounter dampened the experience slightly.", rating: 4, booking_id: 1)
+
+Review.create!(content: "My stay with Astral Escapes was mostly enjoyable, with just a few bumps along the way. The planet offered stunning vistas and fascinating experiences, but the frequent bouts of acid rain put a damper on the outdoor activities. Despite this inconvenience, the accommodations were comfortable, and the staff was attentive to our needs. With a bit of caution and some creative indoor activities, we managed to make the most of our time on this unique planet. Overall, a memorable adventure marred only slightly by the unpredictable weather.", rating: 4, booking_id: 2)
+
+Review.create!(content: "My stay with Astral Escapes was marred by unexpected chaos. While the planet Paradiso initially seemed promising, my experience took a turn for the worse when we were unexpectedly attacked by hostile extraterrestrial beings. The accommodations, while initially comfortable, were ill-prepared for such a situation, leaving us vulnerable and frightened. While I appreciate the efforts of the staff to mitigate the danger, the overall experience was far from enjoyable. Proceed with caution when considering this destination.", rating: 2, booking_id: 2)
+
+Review.create!(content: "My stay with Astral Escapes was utterly disappointing. The planet I booked, Eudoxia, turned out to be inhospitable and downright hostile. From scorching temperatures to toxic atmospheres, it was impossible to enjoy any aspect of the supposed 'getaway.' The accommodations were subpar at best, barely providing shelter from the relentless elements. Save yourself the trouble and steer clear of this disastrous destination. Astral Escapes, you've missed the mark by light-years with this one.", rating: 1, booking_id: 3)
+
+Review.create!(content: "My stay with Astral Escapes exceeded all expectations! While the planet Navnan Ghar may have been unconventional, the culinary experience was out of this world. The local delicacies, including delicious alien bug dishes, were surprisingly delightful and truly a highlight of the trip. Despite initial hesitation, I found myself indulging in flavors I never thought possible. The accommodations were comfortable, and the staff went above and beyond to ensure a memorable stay. A truly stellar experience that I would highly recommend to any adventurous foodie!", rating: 5, booking_id: 4)
+
+
+puts "Finished creating 6 reviews"
 
 puts "---------------------------------"
 
