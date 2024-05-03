8e5b26f6fe81cb4b87c98dcd98d952fdaf5457e8
Chris Collis <theotteryears@me.com>
2024-05-03 10:40:03 +0100

    Review logic fixed

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
+  belongs_to :planet
   validates :content, presence: true
   validates :rating, presence: true, inclusion: { in: 1..5 }
 end
